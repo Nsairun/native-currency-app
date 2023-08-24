de9d8c79a1b2474dfe704c4e1e306eb62acd725a
unknown <emmansairun@gmail.com>
2023-08-24 20:03:52 +0100

    working on the firestore and saving of data in the collection created with firebase

--- pages/LogoPage/LandingPage.tsx
+++ pages/LogoPage/LandingPage.tsx
@@ -1,26 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-const LandingPage = ({navigation}:{ navigation: any }) => {
-    setTimeout(() => {
-        navigation.navigate("LogoPage");
-      }, 1500);
+const LandingPage = ({ navigation }: { navigation: any }) => {
+  setTimeout(() => {
+    navigation.navigate("LogoPage");
+  }, 1500);
+
   return (
     <View style={styles.container}>
       <Text>...</Text>
     </View>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        paddingVertical: 15,
-        marginHorizontal: 16,
-        justifyContent: "space-evenly",
-        backgroundColor: '#fff'
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingVertical: 15,
+    marginHorizontal: 16,
+    justifyContent: "space-evenly",
+    backgroundColor: "#fff",
+  },
+});
